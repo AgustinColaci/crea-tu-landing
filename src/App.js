9789ff891fb968ade6fc0f74c4f38329d4a6416c
Agustín Colaci <agustincolaci@gmail.com>
2021-03-15 20:46:04 -0300

    Desafío Contador

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import ItemListContainer from './components/ItemListContainer';
 import './App.css';
 import NavBar from './components/NavBar';
-
+import AppFunction from './components/AppFunction'
 const App = () => {
   return (
     <>
       <NavBar />
       <ItemListContainer title= "Mis productos" />
+      <AppFunction />
     </>
   );
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,4 +1,5 @@
 import './ItemListContainer.css'
+
 const ItemListContainer = (props)  => (
     <>
         <section>
